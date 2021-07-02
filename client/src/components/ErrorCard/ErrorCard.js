d8a8b3bead3c01c467b9136048539afb001a78cd
KrizpCode <93johan.e@gmail.com>
2021-07-02 14:45:38 +0200

    Clarified error message -Todo: fix responsive bug on card

--- client/src/components/ErrorCard/ErrorCard.js
+++ client/src/components/ErrorCard/ErrorCard.js
@@ -8,6 +8,7 @@ const ErrorCard = ({ results }) => {
         <h2 className="error-card__error-icon"><i className="fas fa-exclamation-triangle"></i></h2>
         <h3 className="error-card__status-code">Error {results.cod}</h3>
         <h4 className="error-card__status-message">{results.message}</h4>
+        <h4 className="error-card__status-message-guide">Please make a new search</h4>
       </div>
     </div>
   )
