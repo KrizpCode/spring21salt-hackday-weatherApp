68f13ec68e7e3351f6c98b25effc515acbbbbdec
KrizpCode <93johan.e@gmail.com>
2021-09-27 17:12:32 +0200

    heroku deployment

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get('/api/cityname/:location', (req, res) => {
 
 app.use('/', express.static(path.join(__dirname, './client/build')));
 
-const port = 8080;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
